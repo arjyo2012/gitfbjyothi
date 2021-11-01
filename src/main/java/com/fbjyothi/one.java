75070e6dceb81d606e5cce1f19e47e2049a17798
jyothiv <jyothiv7984@gmail.com>
2021-10-31 22:09:47 -0400

    second commit with java files

---
+++ src/main/java/com/fbjyothi/one.java
@@ -0,0 +1,13 @@
+package com.fbjyothi;
+
+/**
+ * Hello world!
+ *
+ */
+public class one 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! one java" );
+    }
+}
---
+++ src/main/java/com/fbjyothi/two.java
@@ -0,0 +1,13 @@
+package com.fbjyothi;
+
+/**
+ * Hello world!
+ *
+ */
+public class two 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! two java" );
+    }
+}
